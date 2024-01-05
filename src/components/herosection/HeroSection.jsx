acf51cad3a8067e652d24880661680086ba7f5d2
webAnkit23 <ankitsingh2323@gmail.com>
2024-01-05 16:42:07 +0530

    initial commit

--- src/components/herosection/HeroSection.jsx
+++ src/components/herosection/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './herosection.css'
+import './HeroSection.css'
 import { FaChevronCircleRight } from "react-icons/fa";
 import { FaCheckCircle } from "react-icons/fa";
 
